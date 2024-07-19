53a8b2aabfd297d0cada4e3d9f41c24e1c2b804b
Villi5ta <stankvilius@gmail.com>
2024-07-19 17:12:15 +0300

    added id to schema to work with be

--- src/models/score.js
+++ src/models/score.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const scoreSchema = new mongoose.Schema({
+  id: { type: String, required: true },
   playerName: { type: String, required: true },
   score: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
